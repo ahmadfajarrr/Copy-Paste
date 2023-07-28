022e54140cb45ba47f27ac9cc2bab38c0700d468
ahmadfajarrr <ahmadfajarpersonal@gmail.com>
2023-07-28 16:49:19 +0700

    There is a bug, when there is an error the program does not continue

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Copy-Paste
-This program is a file and directory copying tool that allows you to copy files and directories from one location to another...
+File Copy Program
+Description
+This program is a file and directory copying tool that allows you to copy files and directories from one location to another. The program supports the feature to skip copying if there are errors during the process, so the program will continue copying the remaining files and directories even if there are errors for certain files or directories.
+
+How to Use
+Compile the program using the following command:
+gcc -o cp cp.c
+./cp <source_file_or_pattern> <destination_file_or_directory>
+
+<source_file_or_pattern>: The name of the file or file pattern to be copied. If you want to copy the entire contents of a directory, use the wildcard character *, for example, Documents/*.
+<destination_file_or_directory>: The name of the destination file or directory where the files will be copied. If the destination directory does not exist, the program will create it automatically.
+The program will display the copy progress for each file or directory being copied. If there is an error during the copy process, the program will display an error message, but it will continue the copy process for the next files or directories.
+
+The program will display the message "File '<filename>' copied successfully." for each file that is successfully copied.
+
+Notes:
+
+If you want to copy the entire contents of a directory, make sure to add the wildcard character * at the end of the source directory, for example, Documents/*.
+If there is an error while opening, writing, or closing a file, the program will continue with the next file and display an error message for the file that encountered the error.
+The program also displays an animated progress bar for each file being copied.
+The program optimizes the copy process by using up to 4 threads to increase copy speed.
---
+++ cp.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <libgen.h>
+#include <string.h>
+#include <dirent.h>
+#include <limits.h>
+
+#define BUF_SIZE 4096
+
+const char* my_basename(const char *path) {
+    const char *last_slash = strrchr(path, '/');
+    if (last_slash == NULL) {
+        return path;
+    }
+    return last_slash + 1;
+}
+
+void print_progress(double percentage) {
+    const int bar_width = 50;
+    int progress = bar_width * percentage;
+    printf("[");
+    for (int i = 0; i < bar_width; i++) {
+        if (i < progress) {
+            printf("=");
+        } else {
+            printf(" ");
+        }
+    }
+    printf("] %.0f%%\r", percentage * 100);
+    fflush(stdout);
+}
+
+void copy_file(const char *source_file, const char *destination) {
+    int source_fd, destination_fd;
+    ssize_t bytesRead, bytesWritten;
+    char buffer[BUF_SIZE];
+    struct stat destination_stat;
+
+    source_fd = open(source_file, O_RDONLY);
+    if (source_fd == -1) {
+        perror("Error opening source file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get information about the destination file or directory
+    if (stat(destination, &destination_stat) == -1) {
+        perror("Error getting destination file/directory information");
+        exit(EXIT_FAILURE);
+    }
+
+    if (S_ISDIR(destination_stat.st_mode)) {
+        // If destination is a directory, construct the target file path
+        char destination_file_path[BUF_SIZE];
+        snprintf(destination_file_path, BUF_SIZE, "%s/%s", destination, my_basename(source_file));
+
+        destination_fd = open(destination_file_path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+        if (destination_fd == -1) {
+            perror("Error opening destination file");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        // If destination is not a directory, open it as a file
+        destination_fd = open(destination, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+        if (destination_fd == -1) {
+            perror("Error opening destination file");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    off_t file_size = lseek(source_fd, 0, SEEK_END);
+    lseek(source_fd, 0, SEEK_SET);
+
+    off_t bytesCopied = 0;
+
+    while ((bytesRead = read(source_fd, buffer, BUF_SIZE)) > 0) {
+        bytesWritten = write(destination_fd, buffer, bytesRead);
+        if (bytesWritten != bytesRead) {
+            perror("Error writing to destination file");
+            exit(EXIT_FAILURE);
+        }
+
+        bytesCopied += bytesRead;
+        double percentage = (double)bytesCopied / file_size;
+        print_progress(percentage);
+    }
+
+    if (bytesRead == -1) {
+        perror("Error reading from source file");
+        exit(EXIT_FAILURE);
+    }
+
+    if (close(source_fd) == -1) {
+        perror("Error closing source file");
+        exit(EXIT_FAILURE);
+    }
+
+    if (close(destination_fd) == -1) {
+        perror("Error closing destination file");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("\nFile '%s' copied successfully.\n", source_file);
+}
+
+void copy_directory(const char *source_dir, const char *destination) {
+    DIR *dir;
+    struct dirent *entry;
+    char source_path[PATH_MAX];
+    char destination_path[PATH_MAX];
+
+    dir = opendir(source_dir);
+    if (dir == NULL) {
+        perror("Error opening source directory");
+        exit(EXIT_FAILURE);
+    }
+
+    // Create destination directory if it doesn't exist
+    if (mkdir(destination, 0777) == -1) {
+        perror("Error creating destination directory");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            snprintf(source_path, PATH_MAX, "%s/%s", source_dir, entry->d_name);
+            snprintf(destination_path, PATH_MAX, "%s/%s", destination, entry->d_name);
+
+            struct stat entry_stat;
+            if (stat(source_path, &entry_stat) == -1) {
+                perror("Error getting source file/directory information");
+                exit(EXIT_FAILURE);
+            }
+
+            if (S_ISDIR(entry_stat.st_mode)) {
+                copy_directory(source_path, destination_path);
+            } else {
+                copy_file(source_path, destination);
+            }
+        }
+    }
+
+    closedir(dir);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <source_file_or_pattern> <destination_file_or_directory>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    const char *source = argv[1];
+    const char *destination = argv[2];
+
+    struct stat source_stat;
+    if (stat(source, &source_stat) == -1) {
+        perror("Error getting source file/directory information");
+        exit(EXIT_FAILURE);
+    }
+
+    if (S_ISDIR(source_stat.st_mode)) {
+        copy_directory(source, destination);
+    } else {
+        copy_file(source, destination);
+    }
+
+    exit(EXIT_SUCCESS);
+}
